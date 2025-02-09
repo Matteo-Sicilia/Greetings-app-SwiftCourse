6cd9fc099a95a91dab6730f899dc6a2ba865fb68
Matteo Sicilia <matteo.sicilia@outlook.it>
2025-02-09 16:15:47 +0100

    Add Views and animations

--- Greetings/ContentView.swift
+++ Greetings/ContentView.swift
@@ -9,16 +9,37 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            LinearGradient(
+                colors: [.cyan, .blue, .white],
+                startPoint: .topLeading,
+                endPoint: .bottomTrailing
+            )
+            .opacity(0.7)
+            .ignoresSafeArea(edges: .all)
+            
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                Text("Greetings")
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+                    .padding()
+                    .foregroundStyle(Color.purple)
+                    .background(Color.orange)
+                    .cornerRadius(/*@START_MENU_TOKEN@*/20.0/*@END_MENU_TOKEN@*/)
+                    .shadow(color: .orange, radius: 5, x: 5.0, y: 5.0)
+                    .padding()
+                
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
 }
+
+
