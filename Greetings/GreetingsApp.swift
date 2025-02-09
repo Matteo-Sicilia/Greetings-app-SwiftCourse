a885fe57bbcfdae354462f898848372e25965282
Matteo Sicilia <matteo.sicilia@outlook.it>
2025-02-09 15:45:50 +0100

    Initial Commit

---
+++ Greetings/GreetingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreetingsApp.swift
+//  Greetings
+//
+//  Created by Matteo Sicilia on 09/02/25.
+//
+
+import SwiftUI
+
+@main
+struct GreetingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
